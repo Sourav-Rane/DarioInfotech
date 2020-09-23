264f61bfdd0ad1d09197e8b3008b7b78c80215ab
souravrane <sourav1121997@gmail.com>
2020-09-23 11:43:23 +0530

    link changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,13 +14,9 @@ class Footer extends Component {
 
      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
+          
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 Dario Infotech</li>
            </ul>
 
         </div>
